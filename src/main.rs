01288e16609e61df1da2949683e484ee36ea9066
888 <8fortnite@protonmail.com>
2022-09-15 23:36:16 -0600

    add some rpc client calls

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // use std::error::Error;
 // use solana_program::pubkey::Pubkey;
 use solana_client::rpc_client::RpcClient;
+use solana_sdk::{{clock:: TICKS_PER_DAY}};
 use solana_sdk::signature::Signer; 
 use solana_sdk::{signature::{Keypair}};
 use std::str;
@@ -12,7 +13,7 @@ fn create_keypair() -> Keypair {
 const URL: &str = "https://api.devnet.solana.com";
 
 fn main() {
-    let _rpc_client = RpcClient::new(URL);
+    let rpc_client = RpcClient::new(URL);
 
     // how do i save priv key and use on program restart
     let sender: Keypair = create_keypair();
@@ -21,4 +22,12 @@ fn main() {
     println!("Sender: {:?}", sender.pubkey());
     println!("Sender: {:?}", sender.secret());
     println!("Receiver: {:?}", receiver.pubkey());
+
+    println!("\n==============================\n");
+
+    let epoch_info = rpc_client.get_epoch_info();
+    println!("epock_info: {:?}\n", epoch_info);
+
+    let production = rpc_client.get_block_production();
+    println!("get_block_production: {:?}\n", production);
 }
