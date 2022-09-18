dd8205486edbb493e18306c950700275093dd76c
888 <8fortnite@protonmail.com>
2022-09-18 15:49:55 -0600

    request airdrop

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,36 @@
-// use solana_sdk::{system_transaction, signature::{Keypair, Signature}};
-use solana_sdk::{signature::{Keypair}};
+use std::error::Error;
+use std::fs;
+use solana_client::rpc_client::RpcClient;
+#[allow(unused_imports)]
+use solana_sdk::{signature::{Keypair, Signature}};
+use solana_program::pubkey::Pubkey;
+
+const LAMPORTS_PER_SOL: f64 = 1000000000.0;
 
 pub fn create_keypair() -> Keypair {
     Keypair::new()
 }
+
+pub fn from_key() -> Keypair {
+    let key = fs::read_to_string("key.txt")
+        .expect("The file coud not be read");
+
+    Keypair::from_base58_string(&key)
+}
+
+
+pub fn check_balance(rpc_client: &RpcClient, public_key: &Pubkey) -> Result<f64, Box<dyn Error>> {
+    Ok(rpc_client.get_balance(&public_key)? as f64 / LAMPORTS_PER_SOL)
+}
+
+pub fn request_air_drop(rpc_client: &RpcClient, pub_key: &Pubkey, amount_sol: f64) -> Result<Signature, Box<dyn Error>> {
+    let sig = rpc_client.request_airdrop(&pub_key, (amount_sol * LAMPORTS_PER_SOL) as u64)?;
+    loop {
+        let confirmed = rpc_client.confirm_transaction(&sig)?;
+        if confirmed {
+            break;
+        }
+    }
+    Ok(sig)
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,39 @@
-// use std::error::Error;
-// use solana_program::pubkey::Pubkey;
-use solana_client::rpc_client::RpcClient;
-use solana_sdk::{{clock:: TICKS_PER_DAY}};
-use solana_sdk::signature::Signer; 
-use solana_sdk::{signature::{Keypair}};
 use std::str;
 
-fn create_keypair() -> Keypair {
-    Keypair::new()
-}
+use solana_client::rpc_client::RpcClient;
+// use solana_program::pubkey::Pubkey;
+#[allow(unused_imports)]
+use solana_sdk::{system_transaction, signature::{Keypair, Signature}};
+
+#[allow(unused_imports)]
+use rust_client::{create_keypair, from_key, check_balance, request_air_drop};
+
+use solana_sdk::signature::Signer;
 
 const URL: &str = "https://api.devnet.solana.com";
 
 fn main() {
+    let recovered: Keypair = from_key();
     let rpc_client = RpcClient::new(URL);
-
     // how do i save priv key and use on program restart
-    let sender: Keypair = create_keypair();
-    let receiver: Keypair = create_keypair();
+    // let sender: Keypair = create_keypair();
+    // let receiver: Keypair = create_keypair();
 
-    println!("Sender: {:?}", sender.pubkey());
-    println!("Sender: {:?}", sender.secret());
-    println!("Receiver: {:?}", receiver.pubkey());
+    println!("Recovered: {:?}", recovered.pubkey());
+    println!("Recovered: {:?}", recovered.to_base58_string());
+
+    println!("\n==============================\n");
+
+
+    if let Ok(airdrop_signature) = request_air_drop(&rpc_client, &recovered.pubkey(), 1.0) {
+        println!("Airdrop finished! Signature: {:?}",  airdrop_signature);
+
+        if let Ok(balance) = check_balance(&rpc_client, &recovered.pubkey()) {
+            println!("Sender balance: {:?}", balance);
+        }
+    } else {
+        println!("Airdrop failed");
+    }
 
     println!("\n==============================\n");
 
