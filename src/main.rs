7a35d30b629d3aef06d4c57b57d6cd7b7e98fbb1
888 <8fortnite@protonmail.com>
2022-09-27 01:02:45 -0600

    commit this

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,12 @@ use solana_sdk::{system_transaction, signature::{Keypair, Signature}};
 #[allow(unused_imports)]
 use rust_client::{create_keypair, from_key, check_balance, request_air_drop};
 
-use solana_sdk::signature::Signer;
+// use solana_sdk::signature::Signer;
 
 // #[macro_use]
 // extern crate serde_json;
 
-const URL: &str = "https://api.devnet.solana.com";
+const URL: &str = "https://api.mainnet-beta.solana.com";
 
 // Accounts api was lacking, I couldn't find trx info etc
 
@@ -22,23 +22,23 @@ const URL: &str = "https://api.devnet.solana.com";
 fn main() {
     let rpc_client = RpcClient::new(URL);
 
-    let new_key = create_keypair();
-    let secret =  new_key.to_base58_string();
-    println!("new key secret: {:?}", secret);
+    // let new_key = create_keypair();
+    // let secret =  new_key.to_base58_string();
+    // println!("new key secret: {:?}", secret);
 
-    let recovered: Keypair = from_key("key.txt");
-    let pubkey = recovered.pubkey();
+    // let recovered: Keypair = from_key("key.txt");
+    // let pubkey = recovered.pubkey();
 
-    println!("Recovered: {:?}", recovered.to_base58_string());
-    println!("\n==============================\n");
+    // println!("Recovered: {:?}", recovered.to_base58_string());
+    // println!("\n==============================\n");
 
-    let ga = rpc_client.get_account(&pubkey);                                                                                               
-    println!("get_account: {:?}", ga);
-    println!("\n==============================\n");
+    // let ga = rpc_client.get_account(&pubkey);                                                                                               
+    // println!("get_account: {:?}", ga);
+    // println!("\n==============================\n");
 
-    let ad = rpc_client.get_account_data(&pubkey);                                                                                               
-    println!("get_account_data: {:?}", ad);
-    println!("\n==============================\n");
+    // let ad = rpc_client.get_account_data(&pubkey);                                                                                               
+    // println!("get_account_data: {:?}", ad);
+    // println!("\n==============================\n");
 
     // if let Ok(airdrop_signature) = request_air_drop(&rpc_client, &pubkey, 0.1) {
     //     println!("Airdrop finished! Signature: {:?}",  airdrop_signature);
@@ -64,11 +64,13 @@ fn main() {
     // println!("{}", serde_json::to_string_pretty(&obj).unwrap());
 
     let block = rpc_client.get_block(slot).expect("failed to get block");
-    // println!("{}", serde_json::to_string_pretty(&block).unwrap());
+    println!("block {:?}", block);
 
     println!("\n==============================\n");
 
-    for trx in &block.transactions {
-        println!("trx : {:?}\n", trx.transaction);
-    }
+    // for trx in &block.transactions {
+        // println!("trx : {:?}\n", trx.transaction);
+    // }
+
+
 }
